06d83784f087329f9ac8d2d32e3dc988876a2592
Jed Duffey <jedduffey@gmail.com>
2023-03-18 11:07:10 -0400

    Cleanup/formatting; add openai to package.json

--- test.js
+++ test.js
@@ -1,37 +1,47 @@
-function prompt(numOfQs, category) {
-    return `Give me an array containing ${numOfQs} trivia questions about ${category} and their answers. Each question and answer should be in its own array, with the element at index 0 being the question, and the element at index 1 being the answer.`;
-}
-
+const fs = require('fs');
 const { Configuration, OpenAIApi } = require("openai");
-const { flushSync } = require("react-dom");
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
+console.log(process.env.OPENAI_API_KEY);
+
+function chatGptPrompt(numOfQs, category) {
+    return (
+        `Give me an array containing ${numOfQs} trivia questions about ${category} and their answers. ` +
+        `Each question and answer should be in its own array, with the element at index 0 being the question, and the element at index 1 being the answer.`
+    );
+}
+
 async function fetchTrivia(numOfQs, category) {
-    const foo = prompt(numOfQs, category);
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: foo,
-        max_tokens: 1000,
-    });
-    return response;
+    const prompt = chatGptPrompt(numOfQs, category);
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 1000,
+        });
+        return completion;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 fetchTrivia(3, "pizza")
     .then((res) => {
         const foo = JSON.parse(res.data.choices[0].text);
-        const jsonFoo = JSON.stringify(foo);
-        const folderName = "./Trivia";
         console.log(foo);
-        let fs = require('fs');
+        const jsonFoo = JSON.stringify(foo);
+        console.log(jsonFoo);
+        const folderName = "./Trivia";
         try {
             if (!fs.existsSync(folderName)) {
                 fs.mkdirSync(folderName);
             }
         } catch (err) {
-            console.error(err);
+            console.error(err.data);
         }
         fs.writeFile("./Trivia/sample.json", jsonFoo, function (err) {
             if (err) {
