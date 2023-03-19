f1d08b4e2ab93c0650c6296c237fc9a12ec43eb6
Joshua Rea <josh.rea@gmail.com>
2023-03-19 03:27:10 -0400

    Convert.js code now spits out a .txt file with the data contained in the step2output.jsopn

--- src/convert.js
+++ src/convert.js
@@ -1,12 +1,34 @@
 const fs = require('fs');
+const { parse } = require('path');
 
 const INPUT_PATH = "step2output.json"
+const OUTPUT_DIR = "./trivia";
+const OUTPUT_FILE = "round.txt";
+const OUTPUT_PATH = `${OUTPUT_DIR}/${OUTPUT_FILE}`;
+// const foo = toString(makeRound(INPUT_PATH));
 
-fs.readFile(INPUT_PATH, (err, data) => {
-    if (err) {
-        console.log(`\n🚨🚨🚨 Error reading file 🚨🚨🚨`);
-        console.error(err);
-    }
-    console.log(`\n📄📄📄 File contents (${INPUT_PATH}) 📄📄📄`);
-    console.log(data.toString());
-}); 
+function makeRound(file) {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(`\n🚨🚨🚨 Error reading file 🚨🚨🚨`);
+            console.error(err);
+        }
+        fs.writeFile(OUTPUT_PATH, data, (err) => {
+            if (err) {
+                console.log(`\n🚨🚨🚨 Error writing file 🚨🚨🚨`);
+                console.error(err);
+            }
+            console.log(data.toString());
+        });
+        console.log(`\n📄📄📄 File contents (${INPUT_PATH}) 📄📄📄`);
+        // console.log(data.toString());
+        // const parsedJson = JSON.parse(data);
+        // console.log(parsedJson);
+    });
+}
+
+// function reformat(data) {
+//     data[Object.keys(data)[0]];
+// }
+
+makeRound(INPUT_PATH);
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { Configuration, OpenAIApi } = require("openai");
+const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
